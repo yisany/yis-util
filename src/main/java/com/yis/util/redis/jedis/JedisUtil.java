b5b16797eabffae1b3e0da92ef42a4f9339e990c
yisany <ayqh69@gmail.com>
2020-03-27 15:31:11 +0800

    distributed lock add.

--- src/main/java/com/yis/util/redis/jedis/JedisUtil.java
+++ src/main/java/com/yis/util/redis/jedis/JedisUtil.java
@@ -4,6 +4,9 @@ import com.yis.util.redis.RedisPool;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.params.SetParams;
+
+import java.util.Collections;
 
 /**
  * March, or die.
@@ -14,6 +17,21 @@ import redis.clients.jedis.Jedis;
 public class JedisUtil {
 
     private static final Logger log = LogManager.getLogger(JedisUtil.class);
+
+    private static final String lockKey = "single_lock";
+    // 锁过期时间
+    private static final long internalLockeaseTime = 60 * 1000L;
+    // 获取锁超时时间
+    private static final long timeOut = 60 * 1000L;
+    // 加锁条件
+    private static final SetParams params = SetParams.setParams().nx().px(internalLockeaseTime);
+    // 解锁Lua脚本, 保证原子性
+    private static final String unlockScript =
+            "if redis.call('get',KEYS[1]) == ARGV[1] then" +
+                    "   return redis.call('del',KEYS[1]) " +
+                    "else" +
+                    "   return 0 " +
+                    "end";
 
     /**
      * 设置值, 字符串类型
@@ -79,6 +97,47 @@ public class JedisUtil {
     public static Boolean exists(String key) {
         Object o = execute(j -> j.exists(key));
         return getBoolean(o);
+    }
+
+    /**
+     * 分布式锁, 加锁
+     * @param id
+     * @return
+     */
+    public static boolean lock(String id) {
+        Long start = System.currentTimeMillis();
+        for (;;) {
+            Object execute = execute(j -> j.set(lockKey, id, params));
+            if ("OK".equals(getString(execute))) {
+                // 加锁成功
+                return true;
+            }
+            // 否则循环等待来获取锁, 如果在timeout内仍未获取到锁, 则失败
+            long l = System.currentTimeMillis() - start;
+            if (l > timeOut) {
+                log.error("JedisUtil.lock, get lock error: timeOut, id={}", id);
+                return false;
+            }
+            try {
+                log.info("JedisUtil.lock, try to get lock, id={}", id);
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                log.error("JedisUtil.lock error, e={}", e);
+            }
+        }
+    }
+
+    /**
+     * 分布式锁, 解锁
+     * @param id
+     * @return
+     */
+    public static boolean unlock(String id) {
+        Object execute = JedisUtil.execute(j -> j.eval(unlockScript, Collections.singletonList(lockKey), Collections.singletonList(id)));
+        if ("1".equals(getString(execute))) {
+            return true;
+        }
+        return false;
     }
 
     /**
